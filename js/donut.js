36693fdf6cffc2d5267424f8173e7a52bc12ac4c
GiarcEnoredlac <calderonecraig@gmail.com>
2014-04-07 19:37:25 -0400

    updated donut.js file to accept inputs

--- js/donut.js
+++ js/donut.js
@@ -1,5 +1,27 @@
 $(function(){
 
   //write code here...
+  	function Donut(type, color, tasty) {
+	  	this.type = type;
+	  	this.color = color;
+	  	this.tasty = tasty;
+	  	this.create = function (){
+			$('#donut-list').append('<li>'+
+				'type: '+this.type+ '<br>'+
+				'color: '+this.color+ '<br>'+
+				'tasty: '+this.tasty+'</li>');
+	  	};
 
+	};
+	Donut.all = [];
+
+	$('#create').click(function(){
+		var type = $('#type').val();
+		var color = $('#color').val();
+		var tasty = $('#tasty').val();
+		var donut = new Donut(type, color, tasty);
+		Donut.all.push(donut);
+		donut.create();
+	});
 });
+
